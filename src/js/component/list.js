b649d8cbd698e87e479be3773f8111803eabf24b
kvaldesvallejo <kvaldesvallejo@gmail.com>
2020-04-06 05:47:08 +0000

    Firt commit

---
+++ src/js/component/list.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const List = () => {
+	{
+		/* in the hooks you will have to put a variable and the function
+if you have "todo" you will also need setTodo and useState and will have the initial value of empty string = "" */
+	}
+	return (
+		<div id="container">
+			<h1>TodoList</h1>
+			<input type="text" />
+			<ul>{/*your map function*/}</ul>
+		</div>
+	);
+};
