493dbc495717d3919867f0166d0ee6d9c16dd517
kvaldesvallejo <kvaldesvallejo@gmail.com>
2020-04-11 15:40:55 +0000

    add list and show list

--- src/js/component/list.js
+++ src/js/component/list.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const List = () => {
 	{
 		/* in the hooks you will have to put a variable and the function
 if you have "todo" you will also need setTodo and useState and will have the initial value of empty string = "" */
 	}
+
+	const [todos, setTodos] = useState([]);
+
+	const Enter = e => {
+		if (e.key === "Enter") {
+			//console.log(e.target.value);
+			setTodos(todos.concat(e.target.value));
+		}
+	};
+
 	return (
 		<div id="container">
 			<h1>TodoList</h1>
-			<input type="text" />
-			<ul>{/*your map function*/}</ul>
+			<input type="text" onKeyDown={Enter} />
+			<ul>
+				{todos.map((t, index) => (
+					<li key={index}>{t}</li>
+				))}
+			</ul>
 		</div>
 	);
 };
